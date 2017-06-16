ec65d1d8673efd49c67e8e0c7d4fce191dce985d
Sujit Roy <roy.sujit6@gmail.com>
2017-06-16 12:17:44 +0530

    Add files via upload

---
+++ app/views.py
@@ -0,0 +1,32 @@
+# views.py 
+
+from django.shortcuts import render, HttpResponse
+import requests
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse('Hello World!')
+
+def test(request):
+    return HttpResponse('My second view!')
+
+def profile(request):
+    parsedData = []
+    if request.method == 'POST':
+        username = request.POST.get('user')
+        req = requests.get('https://api.github.com/users/' + username)
+        jsonList = []
+        jsonList.append(req.json())
+        userData = {}
+        for data in jsonList:
+            userData['name'] = data['name']
+            userData['blog'] = data['blog']
+            userData['company'] = data['company']
+            userData['location'] = data['location']
+            userData['public_repos'] = data['public_repos']
+            userData['avatar_url'] = data['avatar_url']
+            userData['followers'] = data['followers']
+            userData['following'] = data['following']
+        parsedData.append(userData)
+    return render(request, 'app/profile.html', {'data': parsedData})
